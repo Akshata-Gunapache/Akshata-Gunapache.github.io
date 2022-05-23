35344930746ef76b93a527d51f51d81de4e2a3a7
AkshataGunapache <agunapache28@gmail.com>
2022-05-23 10:58:16 +0530

    Changes to work section

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,10 +42,26 @@ skillHeaderArr.forEach((element, idx) => {
   });
 });
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container',{
+    selectors:{
+        target:'.work__card'
+    },
+    animation:{
+        duration:300
+    }
+});
 
 
 /* Link active work */ 
 
+const linkWork = document.querySelectorAll('.work__item')
+
+function activework(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click',activework))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
